5596be7d11035d98fd9eab72350c18509085f188
Peter Domokos <petedomokos@gmail.com>
2025-03-15 20:31:01 +0000

    corrected teh way that the examplekey variable is passed to the query in Home

--- client/app/components/visual/page.js
+++ client/app/components/visual/page.js
@@ -2,23 +2,23 @@ import { gql, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import QuadrantsBarChartVisual from '../quadrantsBarChartVisual/page';
 
-const GET_EXAMPLE_DATA = (key) => gql`
-  query getExampleData{
-    exampleData(key:"${key}"){
+const GET_EXAMPLE_DATA = gql`
+  query getExampleData($exampleKey: String!){
+    exampleData(key: $exampleKey){
       data
     }
   }
 `
 
 const Visual = ({ exampleKey="" }) => {
-    //if(!exampleKey) { return null; }
-    console.log("exampleKey", exampleKey)
-    const { data } = useQuery(GET_EXAMPLE_DATA(exampleKey));
+    //console.log("exampleKey", exampleKey)
+    const { data } = useQuery(GET_EXAMPLE_DATA, { variables: { exampleKey }} );
     const [visData, setVisData] = useState(undefined);
     
     useEffect(() => {
         const dataStr = data?.exampleData?.data;
         const dataJSON = dataStr ? JSON.parse(dataStr) : undefined;
+        //console.log("dataJson", dataJSON)
         setVisData(dataJSON)
     }, [data])
 
