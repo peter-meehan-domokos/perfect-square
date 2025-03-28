b5cbfa036400e75fd0ae7f78181f8e6272eb8296
Peter Domokos <petedomokos@gmail.com>
2025-03-28 20:07:17 +0000

    added device detection

--- app/components/home/page.js
+++ app/components/home/page.js
@@ -4,6 +4,7 @@ import { useFetch } from '@/app/api/hooks';
 import Intro from '../intro/page';
 import Header from '../header/page';
 import Visual from '../visual/page';
+import { setUpDeviceDetection } from "@/app/helpers/deviceDetectionHelpers";
 
 const GET_EXAMPLES = `
   query getExamples{
@@ -17,14 +18,27 @@ const Home = ({ }) => {
     const { data } = useFetch(GET_EXAMPLES);
     const [introIsDisplayed, setIntroIsDisplayed] = useState(true);
     const [selectedExampleKey, setSelectedExampleKey] = useState("");
+    const [device, setDevice] = useState("");
 
     useEffect(() => {
       const examples = data?.examples || [];
-      setSelectedExampleKey(examples[0]?.key)
+      setSelectedExampleKey(examples[0]?.key);
     }, [data?.examples])
+
+    useEffect(() => {
+      setUpDeviceDetection();
+      const mob = window.mobileCheck();
+      const mobOrTab = window.mobileAndTabletCheck();
+      if(mobOrTab){
+        setDevice(mob ? "mobile" : "tablet")
+      }else{
+        setDevice("laptop-or-pc")
+      }
+    }, [])
 
     return (
         <div className="home">
+          <h1>{device}</h1>
           {introIsDisplayed ? 
             <Intro 
               closeIntro={() => setIntroIsDisplayed(false)}
