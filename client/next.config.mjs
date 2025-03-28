6fb0d51b5b37be69c2b54d33dc8ef001591f175f
Peter Domokos <petedomokos@gmail.com>
2025-03-28 17:26:54 +0000

    started setting up gihub aciotns config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath:"./perfect-square",
+    output: "export",
+    reactStrictMode: true
+};
 
 export default nextConfig;
